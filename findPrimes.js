c9de3c1ef551e0ea0f07049cc93563305e35a9ae
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-03-09 13:54:20 -0400

    Add error message & tests for input validation

--- findPrimes.js
+++ findPrimes.js
@@ -1,27 +1,30 @@
 let findPrimes = (target) => {
-    let pool = []
-    let primes = [2]
-    let max = Math.sqrt(target)
+    if (target && Number.isInteger(target) && target > 2){
+        let record = []
+        let primes = [2]
+        let max = Math.sqrt(target)
 
-    for(let i = 0; i < target; i++){
-        pool.push(1)
-    }
+        for(let i = 0; i < target; i++){
+            record.push(1)
+        }
 
-    for (let i = 3; i <= max; i += 2){
-        if(pool[i]){
-            for(let j = i * i; j < target; j += i*2){
-                pool[j] = 0
+        for (let i = 3; i <= max; i += 2){
+            if(record[i]){
+                for(let j = i * i; j < target; j += i*2){
+                    record[j] = 0
+                }
             }
         }
-    }
 
-    for (let i=2; i < target; i++){
-        if (pool[i]){
-            primes.push(i)
+        for (let i=3; i < target; i+=2){
+            if (record[i]){
+                primes.push(i)
+            }
         }
+        
+        return primes
     }
-    
-    return primes
+    return "Please enter an integer greater than two"
 }
 
 module.exports = findPrimes
--- test/findPrimes.spec.js
+++ test/findPrimes.spec.js
@@ -6,11 +6,13 @@ describe("#findPrimes", () => {
         expect(findPrimes(100)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
     })
 
-    test("it returns an empty array if given a number less than three", () => {
-        expect(findPrimes(0)).toEqual([]),
-        expect(findPrimes(-6)).toEqual([]),
-        expect(findPrimes(1)).toEqual([]),
-        expect(findPrimes(2)).toEqual([])
+    test("it returns an error message if given a number less than three", () => {
+        expect(findPrimes(0)).toEqual("Please enter an integer greater than two"),
+        expect(findPrimes(-6)).toEqual("Please enter an integer greater than two"),
+        expect(findPrimes(1)).toEqual("Please enter an integer greater than two"),
+        expect(findPrimes(2)).toEqual("Please enter an integer greater than two")
+        expect(findPrimes(2.783)).toEqual("Please enter an integer greater than two")
+        expect(findPrimes("abcd")).toEqual("Please enter an integer greater than two")
     })
 
     test("it returns all primes less than a given large number", () => {
